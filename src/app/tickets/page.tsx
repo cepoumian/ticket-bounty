d2a59ce067f7a2cc848a39cf903ce30e6d6379c0
cepoumian <cepoumian@gmail.com>
2025-06-06 10:36:28 -0600

    Setup Prettier and Prettier Tailwind plugin

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -10,23 +10,23 @@ const TICKET_ICONS = {
 
 const TicketsPage = () => {
   return (
-    <div className="flex-1 flex flex-col gap-y-8">
+    <div className="flex flex-1 flex-col gap-y-8">
       <div>
         <h2 className="text-3xl font-bold tracking-tighter">Tickets Page</h2>
-        <p className="text-sm text-muted-foreground">
+        <p className="text-muted-foreground text-sm">
           All your tickets in one place
         </p>
       </div>
 
-      <div className="flex-1 flex flex-col items-center gap-y-4">
+      <div className="flex flex-1 flex-col items-center gap-y-4">
         {initialTickets.map((ticket) => (
           <div
             key={ticket.id}
-            className="w-full max-w-[420px] p-4 border border-slate-100 gap-y-4"
+            className="w-full max-w-[420px] gap-y-4 border border-slate-100 p-4"
           >
             <div>{TICKET_ICONS[ticket.status]}</div>
-            <h2 className="text-lg truncate">{ticket.title}</h2>
-            <p className="text-sm truncate">{ticket.content}</p>
+            <h2 className="truncate text-lg">{ticket.title}</h2>
+            <p className="truncate text-sm">{ticket.content}</p>
             <Link href={ticketPath(ticket.id)} className="text-sm underline">
               View
             </Link>
