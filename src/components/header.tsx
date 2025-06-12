2b78218f8ef057812f72d433849ae40f00dcec3c
cepoumian <cepoumian@gmail.com>
2025-06-12 10:11:26 -0600

    Implement theme switching

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,11 +2,12 @@ import { LucideKanban } from "lucide-react";
 import Link from "next/link";
 import { buttonVariants } from "@/components/ui/button";
 import { homePath, ticketsPath } from "@/paths";
+import { ThemeSwitcher } from "./theme/theme-switcher";
 
 const Header = () => {
   return (
     <nav className="supports-backdrop-blur:bg-background/60 bg-background/95 fixed top-0 right-0 left-0 z-20 flex w-full justify-between border-b px-5 py-2.5 backdrop-blur">
-      <div>
+      <div className="flex items-center gap-x-2">
         <Link
           href={homePath()}
           className={buttonVariants({
@@ -17,7 +18,8 @@ const Header = () => {
           <h1 className="ml-2 text-lg font-semibold">TicketBounty</h1>
         </Link>
       </div>
-      <div>
+      <div className="flex items-center gap-x-2">
+        <ThemeSwitcher />
         <Link
           href={ticketsPath()}
           className={buttonVariants({
---
+++ src/components/theme/theme-provider.tsx
@@ -0,0 +1,17 @@
+import { ThemeProvider as BaseThemeProvider } from "next-themes";
+
+type ThemeProviderProps = {
+  children: React.ReactNode;
+};
+
+const ThemeProvider = ({ children }: ThemeProviderProps) => {
+  console.log("Where am I rendered? (ThemeProvider)");
+
+  return (
+    <BaseThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      {children}
+    </BaseThemeProvider>
+  );
+};
+
+export { ThemeProvider };
