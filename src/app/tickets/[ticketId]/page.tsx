e954c8e244f3dd58cd631d9873321bedc9f4a242
cepoumian <cepoumian@gmail.com>
2025-06-12 10:18:12 -0600

    Update tickets and individual ticket pages

--- src/app/tickets/[ticketId]/page.tsx
+++ src/app/tickets/[ticketId]/page.tsx
@@ -1,34 +1,23 @@
-import Link from "next/link";
-import { Placeholder } from "@/components/placeholder";
-import { Button } from "@/components/ui/button";
-import { initialTickets } from "@/data";
-import { ticketsPath } from "@/paths";
+import { notFound } from "next/navigation";
+import { TicketItem } from "@/features/ticket/components/ticket-item";
+import { getTicket } from "@/features/ticket/queries/get-ticket";
 
 type TicketPageProps = {
-  params: Promise<{ ticketId: string }>;
+  params: {
+    ticketId: string;
+  };
 };
 
 const TicketPage = async ({ params }: TicketPageProps) => {
-  const { ticketId } = await params;
-  const ticket = initialTickets.find((ticket) => ticket.id === ticketId);
+  const ticket = await getTicket(params.ticketId);
 
   if (!ticket) {
-    return (
-      <Placeholder
-        label="Ticket not found"
-        button={
-          <Button asChild variant="outline">
-            <Link href={ticketsPath()}>Go to Tickets</Link>
-          </Button>
-        }
-      />
-    );
+    notFound();
   }
 
   return (
-    <div>
-      <h2 className="text-lg">Ticket Page {ticket.title}</h2>
-      <p className="text-sm">{ticket.content}</p>
+    <div className="animate-fade-in-from-top flex justify-center">
+      <TicketItem ticket={ticket} isDetail />
     </div>
   );
 };
--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,47 +1,16 @@
-import { LucideCircleCheck, LucideFileText, LucidePencil } from "lucide-react";
-import Link from "next/link";
+import { Suspense } from "react";
 import { Heading } from "@/components/heading";
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { initialTickets } from "@/data";
-import { ticketPath } from "@/paths";
-
-const TICKET_ICONS = {
-  OPEN: <LucideFileText />,
-  IN_PROGRESS: <LucidePencil />,
-  DONE: <LucideCircleCheck />,
-};
+import { Spinner } from "@/components/spinner";
+import { TicketList } from "@/features/ticket/components/ticket-list";
 
 const TicketsPage = () => {
   return (
     <div className="flex flex-1 flex-col gap-y-8">
       <Heading title="Tickets" description="All your tickets in one place" />
 
-      <div className="animate-fade-from-top flex flex-1 flex-col items-center gap-y-4">
-        {initialTickets.map((ticket) => (
-          <Card key={ticket.id} className="w-full max-w-[420px]">
-            <CardHeader>
-              <CardTitle className="flex gap-x-2">
-                <span>{TICKET_ICONS[ticket.status]}</span>
-                <h3 className="truncate text-2xl font-bold">{ticket.title}</h3>
-              </CardTitle>
-            </CardHeader>
-            <CardContent>
-              <p className="line-clamp-3">{ticket.content}</p>
-            </CardContent>
-            <CardFooter>
-              <Link href={ticketPath(ticket.id)} className="text-sm underline">
-                View
-              </Link>
-            </CardFooter>
-          </Card>
-        ))}
-      </div>
+      <Suspense fallback={<Spinner />}>
+        <TicketList />
+      </Suspense>
     </div>
   );
 };
