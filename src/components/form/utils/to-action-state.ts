d2aefbbda02f84458a6364d5ffcf5e9ca811adab
cepoumian <cepoumian@gmail.com>
2025-06-20 12:42:27 -0600

    Create to-action-state util

---
+++ src/components/form/utils/to-action-state.ts
@@ -0,0 +1,62 @@
+import { z } from "zod";
+
+export type ActionState = {
+  status?: "SUCCESS" | "ERROR";
+  message: string;
+  payload?: FormData;
+  fieldErrors?: Record<string, string[] | undefined>;
+  timestamp?: number;
+};
+
+export const EMPTY_ACTION_STATE: ActionState = {
+  message: "",
+  fieldErrors: {},
+  timestamp: Date.now(),
+};
+
+export const fromErrorToActionState = (
+  error: unknown,
+  formData: FormData,
+): ActionState => {
+  if (error instanceof z.ZodError) {
+    // If the error is a ZodError, we return the first error message
+    return {
+      status: "ERROR",
+      message: "",
+      fieldErrors: error.flatten().fieldErrors,
+      payload: formData,
+      timestamp: Date.now(),
+    };
+  } else if (error instanceof Error) {
+    // If the error is a generic Error, we return its message
+    // e.g., database errors, network errors, etc.
+    return {
+      status: "ERROR",
+      message: error.message,
+      fieldErrors: {},
+      payload: formData,
+      timestamp: Date.now(),
+    };
+  } else {
+    // For any other type of error, we return a generic message
+    return {
+      status: "ERROR",
+      message: "An unknown error occurred",
+      fieldErrors: {},
+      payload: formData,
+      timestamp: Date.now(),
+    };
+  }
+};
+
+export const toActionState = (
+  status: ActionState["status"],
+  message: string,
+): ActionState => {
+  return {
+    status,
+    message,
+    fieldErrors: {},
+    timestamp: Date.now(),
+  };
+};
