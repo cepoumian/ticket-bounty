55412cf0834417e6ac0311531304745d86ea03fd
cepoumian <cepoumian@gmail.com>
2025-06-30 19:30:31 -0600

    Set up toast redirect toast and corresponding changes for cookies

---
+++ src/components/redirect-toast.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import { useEffect } from "react";
+import { toast } from "sonner";
+import { consumeCookiedByKey } from "@/actions/cookies";
+
+const RedirectToast = () => {
+  const pathname = usePathname();
+
+  useEffect(() => {
+    const showCookieToast = async () => {
+      const message = await consumeCookiedByKey("toast");
+
+      if (message) {
+        toast.success(message);
+      }
+    };
+
+    showCookieToast();
+  }, [pathname]);
+
+  return null;
+};
+
+export { RedirectToast };
