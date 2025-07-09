148320944beeff075c1223282ecda2bcfa07aa70
cepoumian <cepoumian@gmail.com>
2025-07-09 14:31:08 -0600

    Create TicketMoreMenu component

---
+++ src/features/ticket/components/ticket-more-menu.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import { Ticket, TicketStatus } from "@prisma/client";
+import { LucideTrash } from "lucide-react";
+import { toast } from "sonner";
+import { useConfirmDialog } from "@/components/form/hooks/use-confirm-dialog";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { deleteTicket } from "../actions/delete-ticket";
+import { updateTicketStatus } from "../actions/update-ticket-status";
+import { TICKET_STATUS_LABELS } from "../constants";
+
+type TicketMoreMenuProps = {
+  ticket: Ticket;
+  trigger: React.ReactNode;
+};
+
+const TicketMoreMenu = ({ ticket, trigger }: TicketMoreMenuProps) => {
+  const [deleteButton, deleteDialog] = useConfirmDialog({
+    action: deleteTicket.bind(null, ticket.id),
+    trigger: (
+      <DropdownMenuItem>
+        <LucideTrash className="mr-2 h-4 w-4" />
+        <span>Delete</span>
+      </DropdownMenuItem>
+    ),
+  });
+
+  const handleUpdateTicketStatus = async (value: string) => {
+    const promise = updateTicketStatus(ticket.id, value as TicketStatus);
+
+    toast.promise(promise, {
+      loading: "Updating ticket status...",
+    });
+
+    const result = await promise;
+
+    if (result.status === "ERROR") {
+      toast.error(
+        result.message || "An error occurred while updating the ticket status.",
+      );
+    } else if (result.status === "SUCCESS") {
+      toast.success(result.message || "Ticket status updated successfully.");
+    }
+  };
+
+  const ticketStatusRadioGroupItems = (
+    <DropdownMenuRadioGroup
+      value={ticket.status}
+      onValueChange={handleUpdateTicketStatus}
+    >
+      {(Object.keys(TICKET_STATUS_LABELS) as Array<TicketStatus>).map((key) => (
+        <DropdownMenuRadioItem key={key} value={key}>
+          {TICKET_STATUS_LABELS[key]}
+        </DropdownMenuRadioItem>
+      ))}
+    </DropdownMenuRadioGroup>
+  );
+
+  return (
+    <>
+      {deleteDialog}
+      <DropdownMenu modal={false}>
+        <DropdownMenuTrigger asChild>{trigger}</DropdownMenuTrigger>
+        <DropdownMenuContent side="right" className="w-56">
+          {ticketStatusRadioGroupItems}
+          <DropdownMenuSeparator />
+          {deleteButton}
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  );
+};
+
+export { TicketMoreMenu };
