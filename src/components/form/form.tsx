e4779a6dd09ed929aa35f00b4f0cb9242dcbbcba
cepoumian <cepoumian@gmail.com>
2025-07-09 14:28:11 -0600

    Add optional onSucces and onError props tpo Form component

--- src/components/form/form.tsx
+++ src/components/form/form.tsx
@@ -6,20 +6,35 @@ type FormProps = {
   action: (payload: FormData) => void;
   actionState: ActionState;
   children: React.ReactNode;
+  onSuccess?: (actionState: ActionState) => void;
+  onError?: (actionState: ActionState) => void;
 };
 
-const Form = ({ action, actionState, children }: FormProps) => {
+const Form = ({
+  action,
+  actionState,
+  children,
+  onSuccess,
+  onError,
+}: FormProps) => {
   // Custom hook to handle action state feedback
   useActionFeedback(actionState, {
     onSuccess: ({ actionState }) => {
       if (actionState.message) {
         toast.success(actionState.message);
       }
+      // Call the onSuccess callback if provided.
+      /* if (onSuccess) {
+        onSuccess(actionState);
+      } */
+      // Use optional chaining instead of a conditional to avoid unnecessary checks.
+      onSuccess?.(actionState);
     },
     onError: ({ actionState }) => {
       if (actionState.message) {
         toast.error(actionState.message);
       }
+      onError?.(actionState);
     },
   });
   return (
