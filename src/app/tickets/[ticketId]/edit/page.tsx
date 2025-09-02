64089c3120ae6554c197354208bf9141170a790d
cepoumian <cepoumian@gmail.com>
2025-09-02 10:18:21 -0600

    Create sidebar, breadcrums, tabs and user dropdown

--- src/app/tickets/[ticketId]/edit/page.tsx
+++ src/app/tickets/[ticketId]/edit/page.tsx
@@ -1,9 +1,12 @@
 import { notFound } from "next/navigation";
+import { Breadcrumbs } from "@/components/breadcrumbs";
 import { CardCompact } from "@/components/card-compact";
+import { Separator } from "@/components/ui/separator";
 import { getAuth } from "@/features/auth/queries/get-auth";
 import { isOwner } from "@/features/auth/utils/is-owner";
 import { TicketUpsertForm } from "@/features/ticket/components/ticket-upsert-form";
 import { getTicket } from "@/features/ticket/queries/get-ticket";
+import { homePath, ticketPath } from "@/paths";
 
 type TicketEditPageProps = {
   params: Promise<{
@@ -23,13 +26,24 @@ const TicketEditPage = async ({ params }: TicketEditPageProps) => {
   }
 
   return (
-    <div className="flex flex-1 flex-col items-center justify-center">
-      <CardCompact
-        title="Edit Ticket"
-        description="Edit an existing ticket"
-        content={<TicketUpsertForm ticket={ticket} />}
-        className="animate-fade-in-from-top w-full max-w-[420px]"
+    <div className="flex flex-1 flex-col gap-y-8">
+      <Breadcrumbs
+        breadcrumbs={[
+          { title: "Tickets", href: homePath() },
+          { title: ticket.title, href: ticketPath(ticket.id) },
+          { title: "Edit" },
+        ]}
       />
+
+      <Separator />
+      <div className="flex flex-1 flex-col items-center justify-center">
+        <CardCompact
+          title="Edit Ticket"
+          description="Edit an existing ticket"
+          content={<TicketUpsertForm ticket={ticket} />}
+          className="animate-fade-in-from-top w-full max-w-[420px]"
+        />
+      </div>
     </div>
   );
 };
--- src/components/sidebar/components/sidebar-item.tsx
+++ src/components/sidebar/components/sidebar-item.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
-import { usePathname } from "next/navigation";
 import { cloneElement, ReactElement } from "react";
 import { buttonVariants } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
 import { cn } from "@/lib/utils";
 import { closedClassName } from "../constants";
 import { NavItem } from "../types";
@@ -9,34 +9,35 @@ import { NavItem } from "../types";
 type SidebarItemProps = {
   isOpen: boolean;
   navItem: NavItem;
+  isActive?: boolean;
 };
 
-const SidebarItem = ({ isOpen, navItem }: SidebarItemProps) => {
-  const path = usePathname();
-  const isActive = path === navItem.href;
-
+const SidebarItem = ({ isOpen, navItem, isActive }: SidebarItemProps) => {
   return (
-    <Link
-      href={navItem.href}
-      className={cn(
-        buttonVariants({ variant: "ghost" }),
-        "group relative flex h-12 justify-start",
-        isActive && "bg-muted hover:bg-muted font-bold",
-      )}
-    >
-      {cloneElement(navItem.icon as ReactElement<{ className?: string }>, {
-        className: "h-5 w-5",
-      })}
-      <span
+    <>
+      {navItem.separator && <Separator />}
+      <Link
+        href={navItem.href}
         className={cn(
-          "absolute left-12 text-base duration-200",
-          isOpen ? "hidden md:block" : "w-[78px]",
-          !isOpen && closedClassName,
+          buttonVariants({ variant: "ghost" }),
+          "group relative flex h-12 justify-start",
+          isActive && "bg-muted hover:bg-muted font-bold",
         )}
       >
-        {navItem.title}
-      </span>
-    </Link>
+        {cloneElement(navItem.icon as ReactElement<{ className?: string }>, {
+          className: "h-5 w-5",
+        })}
+        <span
+          className={cn(
+            "absolute left-12 text-base duration-200",
+            isOpen ? "hidden md:block" : "w-[78px]",
+            !isOpen && closedClassName,
+          )}
+        >
+          {navItem.title}
+        </span>
+      </Link>
+    </>
   );
 };
 
