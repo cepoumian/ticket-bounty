d45346a5260818dbb099f7ad5a6e7a7a1c0c9e61
cepoumian <cepoumian@gmail.com>
2025-09-04 12:36:33 -0600

    Typed Search Params [B]

--- src/features/ticket/search-params.ts
+++ src/features/ticket/search-params.ts
@@ -1,8 +1,22 @@
-export type SearchParams = {
-  search: string | string[] | undefined;
-  sort: string | string[] | undefined;
-};
+import { createSearchParamsCache, parseAsString } from "nuqs/server";
 
-// * `?search=hello` (string)
-// * `?search[]=hello&search[]=world` (array)
-// * `?search` (undefined)
+export const searchParamsCache = createSearchParamsCache({
+  search: parseAsString.withDefault(""),
+  sort: parseAsString.withDefault("newest"),
+});
+
+export type ParsedSearchParams = Awaited<
+  ReturnType<typeof searchParamsCache.parse>
+>;
+
+// export type SearchParams = {
+//   search: string | string[] | undefined;
+//   sort: string | string[] | undefined;
+// };
+
+// export type SearchParams = Record<string, string | string[] | undefined>;
+// This type represents the possible shapes of search parameters in the URL.
+// Examples include:
+//  `?search=hello` (string)
+//  `?search[]=hello&search[]=world` (array)
+//  `?search` (undefined)
