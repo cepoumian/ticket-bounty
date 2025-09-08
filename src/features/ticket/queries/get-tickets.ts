b63185fbf07af3546badf7ae4fc718593867be9f
cepoumian <cepoumian@gmail.com>
2025-09-08 11:41:49 -0600

    Pagination - Offset-Based Pagination

--- src/features/ticket/queries/get-tickets.ts
+++ src/features/ticket/queries/get-tickets.ts
@@ -5,15 +5,22 @@ export const getTickets = async (
   userId: string | undefined,
   searchParams: ParsedSearchParams,
 ) => {
-  return await prisma.ticket.findMany({
-    // Safe to do this as Prisma just ignores undefined values
-    where: {
-      userId,
-      title: {
-        contains: searchParams.search,
-        mode: "insensitive",
-      },
+  // Safe to do this as Prisma just ignores undefined values
+  const where = {
+    userId,
+    title: {
+      contains: searchParams.search,
+      mode: "insensitive" as const,
     },
+  };
+
+  const skip = searchParams.page * searchParams.size;
+  const take = searchParams.size;
+
+  return await prisma.ticket.findMany({
+    where,
+    skip,
+    take,
     orderBy: {
       // ...(searchParams.sort === "newest" && { createdAt: "desc" }),
       // ...(searchParams.sort === "bounty" && { bounty: "desc" }),
