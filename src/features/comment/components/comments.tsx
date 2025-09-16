edd877cf84bfe5bbc491f49ba37ef7baeca4456c
cepoumian <cepoumian@gmail.com>
2025-09-15 20:49:59 -0600

    Comments - Create Comment

--- src/features/comment/components/comments.tsx
+++ src/features/comment/components/comments.tsx
@@ -1,4 +1,6 @@
+import { CardCompact } from "@/components/card-compact";
 import { getComments } from "../queries/get-comments";
+import { CommentCreateForm } from "./comment-create-form";
 import { CommentItem } from "./comment-item";
 
 type CommentsProps = {
@@ -9,11 +11,18 @@ const Comments = async ({ ticketId }: CommentsProps) => {
   const comments = await getComments(ticketId);
 
   return (
-    <div className="ml-8 flex flex-col gap-y-2">
-      {comments.map((comment) => (
-        <CommentItem key={comment.id} comment={comment} />
-      ))}
-    </div>
+    <>
+      <CardCompact
+        title="Create Comment"
+        description="A new comment will be created for this ticket"
+        content={<CommentCreateForm ticketId={ticketId} />}
+      />
+      <div className="ml-8 flex flex-col gap-y-2">
+        {comments.map((comment) => (
+          <CommentItem key={comment.id} comment={comment} />
+        ))}
+      </div>
+    </>
   );
 };
 
