c59718e0a77c3b1574647ce9fe1df7e370ce1251
cepoumian <cepoumian@gmail.com>
2025-10-02 12:10:39 -0600

    Implement deliberate over-fetching for hasNextPage

--- src/features/comment/queries/get-comments.ts
+++ src/features/comment/queries/get-comments.ts
@@ -15,10 +15,11 @@ export const getComments = async (
   const take = 2;
   const { user } = await getAuth();
 
-  const [comments, count] = await prisma.$transaction([
+  // eslint-disable-next-line prefer-const
+  let [comments, count] = await prisma.$transaction([
     prisma.comment.findMany({
       where,
-      take,
+      take: take + 1,
       cursor: cursor
         ? { id: cursor.id, createdAt: new Date(cursor.createdAt) }
         : undefined,
@@ -29,6 +30,11 @@ export const getComments = async (
     prisma.comment.count({ where }),
   ]);
 
+  const hasNextPage = comments.length > take;
+
+  // If there is a next page, remove the last item from the comments array since we have one extra item
+  comments = hasNextPage ? comments.slice(0, -1) : comments;
+
   const lastComment = comments.at(-1);
 
   return {
@@ -38,7 +44,7 @@ export const getComments = async (
     })),
     metadata: {
       count,
-      hasNextPage: true, // TODO: Implement hasNextPage
+      hasNextPage,
       cursor: lastComment
         ? {
             id: lastComment?.id,
