ab7ecffc4fcac8d925da3dc9f399cb79ec3851d6
cepoumian <cepoumian@gmail.com>
2026-01-08 11:02:19 -0600

    Forgot Password (Public Page)

--- src/app/password-forgot/page.tsx
+++ src/app/password-forgot/page.tsx
@@ -1,5 +1,17 @@
-const ForgotPasswordPage = () => {
-  return <h2>ForgotPasswordPage</h2>;
+import { CardCompact } from "@/components/card-compact";
+import { PasswordForgotForm } from "@/features/password/components/password-forgot-form";
+
+const PasswordForgotPage = () => {
+  return (
+    <div className="flex flex-1 flex-col items-center justify-center">
+      <CardCompact
+        title="Forgot Password"
+        description="Enter your email to reset your password"
+        content={<PasswordForgotForm />}
+        className="animate-fade-in-from-top w-full max-w-[420px]"
+      />
+    </div>
+  );
 };
 
-export default ForgotPasswordPage;
+export default PasswordForgotPage;
